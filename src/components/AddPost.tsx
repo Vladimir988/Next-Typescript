dee40346a2140b5c62ffa9fa78dfbf78fdb5f260
Vladimir988 <optimist0706@gmail.com>
2024-02-12 16:33:51 +0200

    fix 4

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -13,16 +13,33 @@ const AddPost: FC<Props> = ({savePost}) => {
         } as IPost));
     }
 
+    const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // setValue(e.target.value);
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target?.name]: e.target?.value,
+        } as IPost));
+    }
+
+    const textareaHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target?.name]: e.target?.value,
+        } as IPost));
+    }
+
+    // onChange={e => setPost({...post, title: e.target.value})}
+
     return (
         <form className='Form' onSubmit={(e) => savePost(e, formData as IPost)}>
             <div className='input-wrap'>
                 <div className='Form--field'>
                     <label htmlFor='name'>Title</label>
-                    <input onChange={changeHandler} type='text' id='title' name='title' placeholder='Title'/>
+                    <input onChange={inputHandler} type='text' id='title' name='title' placeholder='Title'/>
                 </div>
                 <div className='Form--field'>
                     <label htmlFor='body'>Description</label>
-                    <textarea onChange={changeHandler} name='body' id='body' rows='5' placeholder='Description'></textarea>
+                    <textarea onChange={textareaHandler} name='body' id='body' rows='5' placeholder='Description'></textarea>
                 </div>
             </div>
             <button
