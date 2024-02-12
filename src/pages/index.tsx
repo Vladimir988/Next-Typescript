c87214d94292d4f86696ef59dd6bcd763dadf4c2
Vladimir988 <optimist0706@gmail.com>
2024-02-12 09:54:37 +0200

    refactoring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,42 @@
-import * as React from 'react';
+import {useState, FormEvent} from "react";
 import AddPost from '@/components/AddPost';
 import Post from '@/components/Post';
-import { InferGetStaticPropsType } from 'next';
+import {InferGetStaticPropsType} from 'next';
 
 const API_URL: string = 'https://jsonplaceholder.typicode.com/posts';
 
-export default function Home({ posts, }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const [postList, setPostList] = React.useState(posts)
-  const addPost = async (e: React.FormEvent, formData: IPost) => {
-    e.preventDefault()
-    const post: IPost = {
-      id: Math.random(),
-      title: formData.title,
-      body: formData.body,
+export default function Home({posts}: InferGetStaticPropsType<typeof getStaticProps>) {
+    const [postList, setPostList] = useState(posts);
+    const addPost = async (e: FormEvent, formData: IPost) => {
+        e.preventDefault()
+        const post: IPost = {
+            id: Math.floor(100 + Math.random() * 900),
+            title: formData.title,
+            body: formData.body,
+        }
+        setPostList([post, ...postList]);
     }
-    setPostList([post, ...postList])
-  }
 
-  const deletePost = async (id: number) => {
-    const posts: IPost[] = postList.filter((post: IPost) => post.id !== id)
-    console.log(posts)
-    setPostList(posts)
-  }
+    const deletePost = async (id: number) => {
+        const posts: IPost[] = postList.filter((post: IPost) => post.id !== id);
+        setPostList(posts);
+    }
 
-  if (!postList) return <h1>Loading...</h1>
+    if (!postList) return <h1>Loading...</h1>
 
-  return (
-      <main className='container'>
-        <h1>My posts</h1>
-        <AddPost savePost={addPost} />
-        {postList.map((post: IPost) => (
-            <Post key={post.id} deletePost={deletePost} post={post} />
-        ))}
-      </main>
-  )
+    return (
+        <main className='container'>
+            <h1>My posts</h1>
+            <AddPost savePost={addPost}/>
+            {postList.map((post: IPost) => (
+                <Post key={post.id} deletePost={deletePost} post={post}/>
+            ))}
+        </main>
+    )
 }
 
 export async function getStaticProps() {
-  const res = await fetch(API_URL)
-  const posts: IPost[] = await res.json()
-  return {
-    props: {
-      posts,
-    },
-  }
+    const res = await fetch(API_URL);
+    const posts: IPost[] = await res.json();
+    return {props: {posts}};
 }
